ababdbaa1c73bab1c85b5f557a176b5b4847a854
Joselu <joseluissgz97@gmail.com>
2024-01-25 19:15:51 +0100

    routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,35 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./layout/home-layout/home-layout.component').then(
+        (m) => m.HomeLayoutComponent
+      ),
+    children: [
+      {
+        path: 'books',
+        loadComponent: () =>
+          import('./pages/books/books.component').then((m) => m.BooksComponent),
+      },
+      {
+        path: 'books-saved',
+        loadComponent: () =>
+          import('./pages/books-saved/books-saved.component').then(
+            (m) => m.BooksSavedComponent
+          ),
+      },
+
+      {
+        path: 'books/:id',
+        loadComponent: () =>
+          import('./pages/book/book.component').then((m) => m.BookComponent),
+      },
+      {
+        path: '**',
+        redirectTo: 'books',
+      },
+    ],
+  },
+];
