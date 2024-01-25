973a9f57193a938d6cc428db082005a44ec49119
Joselu <joseluissgz97@gmail.com>
2024-01-25 19:17:00 +0100

    books saved page

---
+++ src/app/pages/books-saved/books-saved.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-books-saved',
+  standalone: true,
+  imports: [],
+  templateUrl: './books-saved.component.html',
+  styleUrl: './books-saved.component.scss'
+})
+export class BooksSavedComponent {
+
+}
