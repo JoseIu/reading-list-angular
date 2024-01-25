7be45fcfb734f4e2ebc934100f998f3764434e50
Joselu <joseluissgz97@gmail.com>
2024-01-25 19:16:20 +0100

    layout

---
+++ src/app/layout/home-layout/home-layout.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-home-layout',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './home-layout.component.html',
+  styleUrl: './home-layout.component.scss',
+})
+export class HomeLayoutComponent {}
