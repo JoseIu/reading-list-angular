e06881d6bac6b0af333554bbca9e28d0ec36105a
Joselu <joseluissgz97@gmail.com>
2024-01-26 23:00:44 +0100

    getting books and onSaveBook

--- src/app/pages/books/books.component.ts
+++ src/app/pages/books/books.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { Book } from '../../interfaces/book.interface';
+import { BooksServiceService } from '../../services/books-service.service';
 
 @Component({
   selector: 'app-books',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './books.component.html',
-  styleUrl: './books.component.scss'
+  styleUrl: './books.component.scss',
 })
-export class BooksComponent {
+export class BooksComponent implements OnInit {
+  private booksService = inject(BooksServiceService);
+  public books?: Book[];
 
+  ngOnInit(): void {
+    this.getBooks();
+  }
+
+  getBooks(): void {
+    this.booksService.books$.subscribe((books) => {
+      this.books = books.library;
+    });
+
+    console.log(this.books);
+  }
+
+  onSaveBook(ISBN: string): void {
+    this.booksService.saveBook(ISBN);
+  }
 }
