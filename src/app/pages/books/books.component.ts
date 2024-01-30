d5aaf1ad43f606eb625aad997905ffe5e0c119f1
Joselu <joseluissgz97@gmail.com>
2024-01-30 22:21:21 +0100

    refactoring setNewBooks

--- src/app/pages/books/books.component.ts
+++ src/app/pages/books/books.component.ts
@@ -1,32 +1,57 @@
-import { Component, OnInit, inject } from '@angular/core';
+import { Component, OnChanges, OnInit, SimpleChanges, inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { CardBookComponent } from '../../components/card-book/card-book.component';
 import { Book } from '../../interfaces/book.interface';
 import { BooksServiceService } from '../../services/books-service.service';
 
 @Component({
   selector: 'app-books',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, CardBookComponent],
   templateUrl: './books.component.html',
   styleUrl: './books.component.scss',
 })
-export class BooksComponent implements OnInit {
+export class BooksComponent implements OnInit, OnChanges {
   private booksService = inject(BooksServiceService);
   public books?: Book[];
 
   ngOnInit(): void {
-    this.getBooks();
+    //check if we have saved books
+    const savedBooks: string[] = this.booksService.getSavedBooks();
+    console.log('SAVED-BOOKS', savedBooks.length);
+
+    if (savedBooks.length === 0) {
+      this.getBooks();
+
+      return;
+    }
+    this.setNewBooks(savedBooks);
+    // this.getBooks();
+  }
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('CHANGES', changes);
   }
 
-  getBooks(): void {
-    this.booksService.books$.subscribe((books) => {
-      this.books = books.library;
-    });
+  public getBooks(): void {
+    this.booksService.books$.subscribe((books) => (this.books = books.library));
 
-    console.log(this.books);
+    // console.log('GET-BOOKS', this.books);
   }
 
-  onSaveBook(ISBN: string): void {
+  public onSaveBook(ISBN: string): void {
+    //save book in localStorage
     this.booksService.saveBook(ISBN);
+    //update and save _savedBooks
+    this.booksService.updateSavedBooks(ISBN);
+  }
+
+  //Only if we have saved books
+  public setNewBooks(savedBooks: string[]): void {
+    if (savedBooks.length) {
+      console.log(savedBooks);
+      this.booksService.books$.subscribe((books) => {
+        this.books = books.library.filter((book) => !savedBooks.includes(book.book.ISBN));
+      });
+    }
   }
 }
