5aa984eb3c502f5fc9c7e17188209cb6a5a1032c
Joselu <joseluissgz97@gmail.com>
2024-02-01 17:18:55 +0100

    arrow back component

---
+++ src/app/components/arrow-back/arrow-back.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'arrow-back',
+  standalone: true,
+  imports: [],
+  templateUrl: './arrow-back.component.html',
+  styleUrl: './arrow-back.component.scss',
+})
+export class ArrowBackComponent {}
