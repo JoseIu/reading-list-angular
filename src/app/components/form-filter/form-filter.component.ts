d3115198950fb2378df61c9a3be549c6730fb2ee
Joselu <joseluissgz97@gmail.com>
2024-02-01 17:20:16 +0100

    pageRange

--- src/app/components/form-filter/form-filter.component.ts
+++ src/app/components/form-filter/form-filter.component.ts
@@ -12,22 +12,26 @@ import { BooksServiceService } from '../../services/books-service.service';
 })
 export class FormFilterComponent implements OnInit, OnDestroy {
   public booksService = inject(BooksServiceService);
+  public pageRange: number = 10;
   myForm = new FormGroup({
     search: new FormControl(''),
-    range: new FormControl<number>(0),
+    range: new FormControl<number>(10),
     sortBy: new FormControl('all'),
   });
 
   ngOnInit(): void {
     this.myForm.valueChanges.subscribe((formValues) => {
       const { search, range, sortBy } = formValues;
+
       const sortByy = sortBy || '';
 
       const rangeToInteger = Number(range);
+      this.pageRange = rangeToInteger;
 
       this.booksService.filterBooks(search?.toLocaleLowerCase(), rangeToInteger, sortByy);
     });
   }
+
   ngOnDestroy(): void {
     this.myForm.reset();
   }
