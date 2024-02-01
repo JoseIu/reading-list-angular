38d40d7cf2c9d83eaba68662f75faa067e67ecc3
Joselu <joseluissgz97@gmail.com>
2024-02-01 11:32:58 +0100

    importing FormFilterComponent

--- src/app/pages/books/books.component.ts
+++ src/app/pages/books/books.component.ts
@@ -1,24 +1,24 @@
-import { Component, OnChanges, OnInit, SimpleChanges, inject } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CardBookComponent } from '../../components/card-book/card-book.component';
+import { FormFilterComponent } from '../../components/form-filter/form-filter.component';
 import { Book } from '../../interfaces/book.interface';
 import { BooksServiceService } from '../../services/books-service.service';
 
 @Component({
   selector: 'app-books',
   standalone: true,
-  imports: [RouterModule, CardBookComponent],
+  imports: [RouterModule, CardBookComponent, FormFilterComponent],
   templateUrl: './books.component.html',
   styleUrl: './books.component.scss',
 })
-export class BooksComponent implements OnInit, OnChanges {
+export class BooksComponent implements OnInit {
   private booksService = inject(BooksServiceService);
   public books?: Book[];
 
   ngOnInit(): void {
     //check if we have saved books
     const savedBooks: string[] = this.booksService.getSavedBooks();
-    console.log('SAVED-BOOKS', savedBooks.length);
 
     if (savedBooks.length === 0) {
       this.getBooks();
@@ -26,16 +26,10 @@ export class BooksComponent implements OnInit, OnChanges {
       return;
     }
     this.setNewBooks(savedBooks);
-    // this.getBooks();
-  }
-  ngOnChanges(changes: SimpleChanges): void {
-    console.log('CHANGES', changes);
   }
 
   public getBooks(): void {
     this.booksService.books$.subscribe((books) => (this.books = books.library));
-
-    // console.log('GET-BOOKS', this.books);
   }
 
   public onSaveBook(ISBN: string): void {
@@ -45,10 +39,9 @@ export class BooksComponent implements OnInit, OnChanges {
     this.booksService.updateSavedBooks(ISBN);
   }
 
-  //Only if we have saved books
+  //Set new books when we add to saved-books (Only if we have saved books)
   public setNewBooks(savedBooks: string[]): void {
     if (savedBooks.length) {
-      console.log(savedBooks);
       this.booksService.books$.subscribe((books) => {
         this.books = books.library.filter((book) => !savedBooks.includes(book.book.ISBN));
       });
