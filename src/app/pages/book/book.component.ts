b6620ab7055513cbab80353756f8ab2c15a5bd08
Joselu <joseluissgz97@gmail.com>
2024-02-01 17:21:10 +0100

    page for each book when we clicked in a book

--- src/app/pages/book/book.component.ts
+++ src/app/pages/book/book.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { ActivatedRoute, RouterModule } from '@angular/router';
+import { switchMap } from 'rxjs';
+import { ArrowBackComponent } from '../../components/arrow-back/arrow-back.component';
+import { Book } from '../../interfaces/book.interface';
+import { BooksServiceService } from '../../services/books-service.service';
 
 @Component({
   selector: 'app-book',
   standalone: true,
-  imports: [],
+  imports: [ArrowBackComponent, RouterModule],
   templateUrl: './book.component.html',
-  styleUrl: './book.component.scss'
+  styleUrl: './book.component.scss',
 })
-export class BookComponent {
+export class BookComponent implements OnInit {
+  private activateRoute = inject(ActivatedRoute);
+  private booksService = inject(BooksServiceService);
 
+  public book?: Book;
+  ngOnInit(): void {
+    this.activateRoute.params
+      .pipe(switchMap(({ id }) => this.booksService.getBookByISBN(id)))
+      .subscribe((book) => {
+        this.book = book;
+      });
+  }
 }
